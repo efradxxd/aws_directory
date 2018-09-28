02a3f5d4b62da32ead3a98f7b25f3c3b561e53bd
Marco Mercado Lugo <marcomercadolugo@MacBook-Pro-de-Marco.local>
2018-09-28 13:40:39 -0500

    Version 1.0

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
 var express = require('express');
 var aws = require('aws-sdk');
 
-aws.config.update({region:'us-east-1'});
+aws.config.update({ region: 'us-east-1' });
 
 var app = express();
 var request = require('request');
 var teamMembers;
+var urlAPI = 'https://5kjpo28p8l.execute-api.us-east-2.amazonaws.com/dev/CloudTeam';
 
 app.set('view engine', 'ejs');
-
-request.get('https://uhmoxvk5s0.execute-api.us-east-1.amazonaws.com/dev/DirectoryFunc', (err, res, body) => {
-    if(err) {
+request.get(urlAPI, (err, res, body) => {
+    if (err) {
         return console.dir(err);
     }
-    console.log(JSON.parse(body));
+
     teamMembers = JSON.parse(body);
     teamMembers = teamMembers.reverse();
 
 });
 
+app.get('/', function (req, res) {
+    request.get(urlAPI, (err, res, body) => {
+        if (err) {
+            return console.dir(err);
+        }
 
-app.get('/', function(req, res) {
+        teamMembers = JSON.parse(body);
+        teamMembers = teamMembers.reverse();
+
+    });
     res.render('pages/index', {
         teamMembers: teamMembers
     });
@@ -30,4 +38,6 @@ app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
 console.log("Directory app runing on port 3000");
+
+
 module.exports = app;
